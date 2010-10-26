b72077db8da561e017764e551e5bd96256035f3c
Andrew Watts <awatts@bcs.rochester.edu>
2010-10-26 10:02:06 -0400

    Trying a CSS based approach that might work better and not involve scriping

--- js_includes/RegionedSentence.js
+++ js_includes/RegionedSentence.js
@@ -109,7 +109,8 @@ $.widget("ui.RegionedSentence", {
         this.sentDivs[0] = $(document.createElement("div")).text("");
         this.mainDiv.append(this.sentDivs[0]);
         this.sentDivs[0].addClass(this.cssPrefix + "sentence");
-        this.sentDivs[0].disableTextSelect();
+        //this.sentDivs[0].disableTextSelect();
+        this.sentDivs[0].attr('unselectable', 'on');
 
         this.wordSpans = new Array(this.words.length);
         this.wsnjq = new Array(this.words.length); // 'word spans no jQuery'.
@@ -125,7 +126,8 @@ $.widget("ui.RegionedSentence", {
                 this.sentDivs[divNo] = $(document.createElement("div")).text("");
                 this.mainDiv.append(this.sentDivs[divNo]);
                 this.sentDivs[divNo].addClass(this.cssPrefix + "sentence");
-                this.sentDivs[divNo].disableTextSelect();
+                //this.sentDivs[divNo].disableTextSelect();
+                this.sentDivs[divNo].attr('unselectable', 'on');
             }
         }
 
